b93023c886314bf3548b21feca23d26b6ebf90b4
fufeng1226 <15281205719@163.com>
2020-03-16 15:52:57 +0800

    dependency aware injection

---
+++ thinking-in-spring/dependency-injection/src/main/java/org/lcydream/ioc/dependency/injection/aware/AwareDependencyInjection.java
@@ -0,0 +1,44 @@
+package org.lcydream.ioc.dependency.injection.aware;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * 通过{@link org.springframework.beans.factory.Aware}  接口实现注入
+ */
+public class AwareDependencyInjection implements BeanFactoryAware, ApplicationContextAware {
+
+    private static BeanFactory beanFactory;
+
+    private static ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        //创建一个Spring 应用上下文
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext();
+        //注册配置类
+        applicationContext.register(AwareDependencyInjection.class);
+        //启动应用上下文
+        applicationContext.refresh();
+
+        System.out.println(applicationContext == AwareDependencyInjection.applicationContext);
+        System.out.println(applicationContext.getBeanFactory() == AwareDependencyInjection.beanFactory);
+
+        //关闭应用上下文
+        applicationContext.close();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        AwareDependencyInjection.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        AwareDependencyInjection.applicationContext = applicationContext;
+    }
+}
--- thinking-in-spring/dependency-injection/src/main/resources/readme.txt
+++ thinking-in-spring/dependency-injection/src/main/resources/readme.txt
@@ -5,6 +5,17 @@
     方法 @Autowired public void user(User user){this.user = user;}
     接口回调 class userBean implements BeanFactoryAware{}
 
+接口回调注入
+    Aware系列接口回调
+        BeanFactoryAware                获取IOC容器 - BeanFactory
+        ApplicationContextAware         获取Spring应用上下文 - ApplicationContext 对象
+        EnvironmentAware                获取Environment对象
+        ResourceLoaderAware             获取资源加载器 对象 - ResourceLoader
+        BeanClassLoaderAware            获取加载当前Bean Class的 ClassLoader
+        BeanNameAware                   获取当前Bean的名称
+        MessageSourceAware              获取MessageSource对象,用于Spring国际化
+        ApplicationEventPublisherAware  获取ApplicationEventPublisher对象，用于Spring事件
+        EmbeddedValueResolverAware      获取StringValueResolver对象，用于占位符处理
 
 
 
