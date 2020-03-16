c9509800eb638d173dfcfdf90670b3b425f6c796
fufeng1226 <15281205719@163.com>
2020-03-16 16:19:19 +0800

    type for java Object dependency injection

--- thinking-in-spring/ioc-container-overview/src/main/java/org/lcydream/domain/User.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/lcydream/domain/User.java
@@ -1,5 +1,9 @@
 package org.lcydream.domain;
 
+import org.lcydream.enums.City;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 /**
  *  用户类
  */
@@ -8,6 +12,13 @@ public class User {
     private Long id;
 
     private String name;
+
+    private City city;
+
+    /**
+     * 基于 {@link ClassPathResource} 加载配置文件
+     */
+    private Resource configFileLocal;
 
     public Long getId() {
         return id;
@@ -25,11 +36,29 @@ public class User {
         this.name = name;
     }
 
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Resource getConfigFileLocal() {
+        return configFileLocal;
+    }
+
+    public void setConfigFileLocal(Resource configFileLocal) {
+        this.configFileLocal = configFileLocal;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", city=" + city +
+                ", configFileLocal=" + configFileLocal +
                 '}';
     }
 
@@ -37,6 +66,7 @@ public class User {
         User user = new User();
         user.setId(1L);
         user.setName("fufeng");
+        user.setCity(City.CHENGDU);
         return user;
     }
 }
---
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/lcydream/enums/City.java
@@ -0,0 +1,12 @@
+package org.lcydream.enums;
+
+/**
+ * 城市枚举
+ */
+public enum City {
+
+    BEIJING,
+    SHANGHAI,
+    CHENGDU
+
+}
