4ca46b6b7c1813adedd6f23f9b1ea83a59492c80
fufeng1226 <15281205719@163.com>
2020-03-25 22:16:10 +0800

    spring boot 加上自动配置的注解条件

--- thinking-in-spring-boot/embed-container/src/main/java/org/lcydream/configuration/WebServletAutoConfiguration.java
+++ thinking-in-spring-boot/embed-container/src/main/java/org/lcydream/configuration/WebServletAutoConfiguration.java
@@ -1,8 +1,10 @@
 package org.lcydream.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@ConditionalOnWebApplication
 @Configuration
 @Import(WebServletConfiguration.class)
 public class WebServletAutoConfiguration {
