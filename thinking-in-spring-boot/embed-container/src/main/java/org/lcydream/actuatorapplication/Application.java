f110281ff45a2e8bc645be3298849629effb76e2
fufeng1226 <15281205719@163.com>
2020-03-27 09:56:02 +0800

    spring boot 外部化配置优先级

--- thinking-in-spring-boot/embed-container/src/main/java/org/lcydream/actuatorapplication/Application.java
+++ thinking-in-spring-boot/embed-container/src/main/java/org/lcydream/actuatorapplication/Application.java
@@ -1,10 +1,23 @@
 package org.lcydream.actuatorapplication;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * management.endpoints.web.exposure.include=*
+ * mvn spring-boot:run -Dmanagement.endpoints.web.exposure.include=beans,conditions,env
+ *
+ *  java -jar app.jar --name="luo" 可以通过外部命令行参数进行值覆盖
+ *
+ *  配置优先级:
+ *   https://docs.spring.io/spring-boot/docs/2.2.5.RELEASE/reference/html/spring-boot-features.html#boot-features-external-config
+ */
 @SpringBootApplication
 public class Application {
+
+    @Value("${name}")
+    private String name;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
