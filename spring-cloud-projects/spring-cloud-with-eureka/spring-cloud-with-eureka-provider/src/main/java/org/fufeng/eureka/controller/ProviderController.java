b384eac9b8349b56f2079ea21cf747f87cbb5c14
MagicLuo <15281205719@163.com>
2020-08-26 16:30:42 +0800

    eureka 服务提供和服务发现 feign client 和 TestTemplate应用

---
+++ spring-cloud-projects/spring-cloud-with-eureka/spring-cloud-with-eureka-provider/src/main/java/org/fufeng/eureka/controller/ProviderController.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2020 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-spring-boot
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-08-26
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.fufeng.eureka.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @program: thinking-in-spring-boot
+ * @description: 提供者 REST API
+ * @author: <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @create: 2020-08-26
+ */
+@Slf4j
+@RestController
+public class ProviderController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/hello")
+    public String hello(){
+        final List<String> services = discoveryClient.getServices();
+        services.forEach(log::info);
+        return "hello spring cloud";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        final List<String> services = discoveryClient.getServices();
+        services.forEach(log::info);
+        return "info spring cloud";
+    }
+}
