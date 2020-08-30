14448be546ea40f46a5432b17cad1fe300d86f56
MagicLuo <15281205719@163.com>
2020-08-30 16:47:44 +0800

    spring livecycle

---
+++ thinking-in-spring/application-context-lifecycle/src/main/java/org/fufeng/context/lifucycle/CustomLifecycle.java
@@ -0,0 +1,49 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2019 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-spring-boot
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-08-30
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.fufeng.context.lifucycle;
+
+import org.springframework.context.Lifecycle;
+
+/**
+ * @program: thinking-in-spring-boot
+ * @description: 自定义 生命周期实现
+ * @author: <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @create: 2020-08-30
+ * @see Lifecycle
+ */
+public class CustomLifecycle implements Lifecycle {
+
+    private volatile boolean running;
+
+    @Override
+    public void start() {
+        this.running = true;
+        System.out.println("application context is running");
+    }
+
+    @Override
+    public void stop() {
+        this.running = false;
+        System.out.println("application context is stopping");
+    }
+
+    @Override
+    public boolean isRunning() {
+        return this.running;
+    }
+}
---
+++ thinking-in-spring/application-context-lifecycle/src/main/java/org/fufeng/context/lifucycle/CustomLifecycleInfo.java
@@ -0,0 +1,51 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2019 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-spring-boot
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-08-30
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.fufeng.context.lifucycle;
+
+import org.springframework.context.support.GenericApplicationContext;
+
+import static org.springframework.beans.factory.support.BeanDefinitionBuilder.rootBeanDefinition;
+
+/**
+ * @program: thinking-in-spring-boot
+ * @description: 自定义lifecycle 示例
+ * @author: <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @create: 2020-08-30
+ */
+public class CustomLifecycleInfo {
+
+    public static void main(String[] args) {
+        // 创建应用上下文
+        GenericApplicationContext context = new GenericApplicationContext();
+
+        // 注册上下文
+        context.registerBeanDefinition("customLifecycle",
+                rootBeanDefinition(CustomLifecycle.class).getBeanDefinition());
+
+        // 刷新应用上下文
+        context.refresh();
+
+        context.start();
+
+        context.stop();
+
+        // 关闭应用上下文
+        context.close();
+    }
+
+}
