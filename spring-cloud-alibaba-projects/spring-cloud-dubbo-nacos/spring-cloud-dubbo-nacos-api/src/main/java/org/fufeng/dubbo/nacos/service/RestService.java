3f81ff35a63a64d49330cc76f0e27c71ab42e5e1
MagicLuo <15281205719@163.com>
2020-10-16 17:21:43 +0800

    spring cloud use dubbo in network layer

---
+++ spring-cloud-alibaba-projects/spring-cloud-dubbo-nacos/spring-cloud-dubbo-nacos-api/src/main/java/org/fufeng/dubbo/nacos/service/RestService.java
@@ -0,0 +1,46 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2019 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-spring-boot
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-10-16
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.fufeng.dubbo.nacos.service;
+
+import org.fufeng.dubbo.nacos.domain.User;
+
+import java.util.Map;
+
+/**
+ * @author <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @program thinking-in-spring-boot
+ * @description RestTemple 行为接口 , Feign 调用
+ * @create 2020-10-16
+ */
+public interface RestService {
+
+    String param(String param);
+
+    String params(int intValue, String stringValue);
+
+    String headers(String heard, String heard_, Integer param);
+
+    String pathVariables(String path, String path_,String param);
+
+    String form(String form);
+
+    User requestBodyMap(Map<String,Object> data,String param);
+
+    Map<String,Object> requestBodyUser(User user);
+
+}
---
+++ spring-cloud-alibaba-projects/spring-cloud-dubbo-nacos/spring-cloud-dubbo-nacos-api/src/main/java/org/fufeng/dubbo/nacos/util/LoggerUtils.java
@@ -0,0 +1,57 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2019 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-spring-boot
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-10-16
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.fufeng.dubbo.nacos.util;
+
+import org.apache.dubbo.rpc.RpcContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @program thinking-in-spring-boot
+ * @description 日志组件工具
+ * @create 2020-10-16
+ */
+public class LoggerUtils {
+
+    /**
+     *  日志组件抽象
+     */
+    private static final Logger logger = LoggerFactory.getLogger(LoggerUtils.class);
+
+    /**
+     *  打印方法实现
+     * @param url url
+     * @param result 结果
+     */
+    public static void log(String url,Object result){
+        // 构建远程过程调用产生的消息
+        String resultCallMessage = String.format("The client[%s] uses '%s' protocol to call %s : %s",
+                RpcContext.getContext().getRemoteHostName(),
+                RpcContext.getContext().getUrl() == null ? "N/A":RpcContext.getContext().getUrl().getProtocol(),
+                url,
+                result
+        );
+
+        // 是否开启info级别的日志打印
+        if (logger.isInfoEnabled()){
+            logger.info(resultCallMessage);
+        }
+    }
+
+}
