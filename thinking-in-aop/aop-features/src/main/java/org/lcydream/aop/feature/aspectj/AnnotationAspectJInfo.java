2b1c0df845c688f41bc6dfe3bc94573b9f46ac66
fufeng <15281205719@163.com>
2021-02-28 19:58:25 +0800

    aspectj aop 特性

---
+++ thinking-in-aop/aop-features/src/main/java/org/lcydream/aop/feature/aspectj/AnnotationAspectJInfo.java
@@ -0,0 +1,47 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2019 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: spring-cloud-alibaba-projects
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2021-02-28
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.lcydream.aop.feature.aspectj;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @program spring-cloud-alibaba-projects
+ * @description AspectJ 示例
+ * @create 2021-02-28
+ */
+@Aspect // 声明为 Aspect 切面
+@Configuration // Configuration Class
+@EnableAspectJAutoProxy // 激活 AspectJ 注解自动代理
+public class AnnotationAspectJInfo {
+
+    public static void main(String[] args) {
+        final AnnotationConfigApplicationContext configApplicationContext =
+                new AnnotationConfigApplicationContext();
+        configApplicationContext.register(AnnotationAspectJInfo.class);
+        configApplicationContext.refresh();
+
+        final AnnotationAspectJInfo aspectJInfo = configApplicationContext.getBean(AnnotationAspectJInfo.class);
+
+        configApplicationContext.close();
+    }
+
+}
---
+++ thinking-in-aop/aop-features/src/main/resources/AOP特性.md
@@ -0,0 +1,27 @@
+### @AspectJ 注解驱动
+```text
+1、激活@AspectJ模块
+    1、注解激活 - @EnableAspectJAutoProxy
+    2、XML配置 - <aop:aspectj-autoproxy/>
+2、声明Aspectj
+    @Aspectj    
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
