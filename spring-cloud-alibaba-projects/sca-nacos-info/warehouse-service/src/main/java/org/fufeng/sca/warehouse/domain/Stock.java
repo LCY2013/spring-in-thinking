979a526e78face093d4ddbffc09f5157193e99d7
fufeng <15281205719@163.com>
2021-03-17 17:17:24 +0800

    仓储服务

---
+++ spring-cloud-alibaba-projects/sca-nacos-info/warehouse-service/src/main/java/org/fufeng/sca/warehouse/domain/Stock.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2019 Ramostear.All Rights Reserved.
+ *
+ * ProjectName: spring-cloud-alibaba-projects
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2021-03-17
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.fufeng.sca.warehouse.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @program spring-cloud-alibaba-projects
+ * @description sku 库存商品对象
+ * @create 2021-03-17
+ * @since 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock {
+    /**
+     *  商品类目编号
+     */
+    private Long skuId;
+
+    /**
+     * 商品与品类名称
+     */
+    private String title;
+
+    /**
+     * 库存数量
+     */
+    private Integer quantity;
+
+    /**
+     * 单位
+     */
+    private String unit;
+
+    /**
+     * 描述信息
+     */
+    private String description;
+
+    public Stock(Long skuId, String title, Integer quantity, String unit) {
+        this.skuId = skuId;
+        this.title = title;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+}
