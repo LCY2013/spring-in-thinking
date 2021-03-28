0a783c32eea9f9b18227065f86ebd901b31087c3
fufeng <15281205719@163.com>
2021-03-28 20:09:36 +0800

    spring aop org.aspectj.lang.annotation.Pointcut Around 示例

--- thinking-in-aop/aop-features/src/main/java/org/lcydream/aop/feature/aspect/AspectConfiguration.java
+++ thinking-in-aop/aop-features/src/main/java/org/lcydream/aop/feature/aspect/AspectConfiguration.java
@@ -1,5 +1,7 @@
 package org.lcydream.aop.feature.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -29,6 +31,19 @@ public class AspectConfiguration {
     @Before("doAnyMethod()")
     public void beforeAnyPublicMethod() {
         System.out.println("@beforeAnyPublicMethod at any public method.");
+    }
+
+    /**
+     *  定义Around通知，利用pointcut过滤执行方法
+     *
+     * around 和 before 同时在时有何区别？
+     *  around需要主动去操作执行目标方法，before则不需要就会执行目标方法
+     *  两个同时针对某一个Pointcut时，around优先执行，并且执行了pjp.proceed()后面的其他（before和目标方法操作...）才会生效
+     */
+    @Around("doAnyMethod()")
+    public Object aroundAnyPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("@aroundAnyPublicMethod at any public method.");
+        return pjp.proceed();
     }
 
 }
