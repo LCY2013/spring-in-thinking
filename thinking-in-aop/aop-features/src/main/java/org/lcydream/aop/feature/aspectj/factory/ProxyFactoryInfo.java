eac8aaca7c8ef1fbfa0602eedf7c2e1977a73984
fufeng <15281205719@163.com>
2021-03-28 11:29:28 +0800

    spring aop org.springframework.aop.framework.ProxyFactory 示例

---
+++ thinking-in-aop/aop-features/src/main/java/org/lcydream/aop/feature/aspectj/factory/ProxyFactoryInfo.java
@@ -0,0 +1,29 @@
+package org.lcydream.aop.feature.aspectj.factory;
+
+import org.lcydream.aop.feature.aspectj.proxyfactory.interceptor.EchoServiceMethodInterceptor;
+import org.lcydream.aop.staticproxy.DefaultEchoService;
+import org.lcydream.aop.staticproxy.EchoService;
+import org.springframework.aop.framework.ProxyFactory;
+
+/**
+ * @author luocy
+ * @description ProxyFactory 信息
+ * @program customer-service
+ * @create 2021-03-28
+ * @since 1.0
+ */
+public class ProxyFactoryInfo {
+
+    public static void main(String[] args) {
+        // 构建需要被代理的对象
+        final DefaultEchoService echoService = new DefaultEchoService();
+        // 构建代理工厂
+        final ProxyFactory proxyFactory = new ProxyFactory(echoService);
+        // 添加代理的拦截器，也就是advice
+        proxyFactory.addAdvice(new EchoServiceMethodInterceptor());
+        // 获取代理对象
+        final EchoService echo = (EchoService)proxyFactory.getProxy();
+        System.out.println((echo.cil("fufeng")));
+    }
+
+}
