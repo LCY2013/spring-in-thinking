2b29856bd7f39e6a71879dde4414c30baae99ee9
fufeng <15281205719@163.com>
2021-04-07 21:27:45 +0800

    skywalking apm 集成

--- spring-cloud-alibaba-projects/sca-nacos-info/c-service/src/main/java/org/fufeng/sleuth/C_Application.java
+++ spring-cloud-alibaba-projects/sca-nacos-info/c-service/src/main/java/org/fufeng/sleuth/C_Application.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author luocy
  * @description c服务应用
  * skywalking 启动参数如下：
- * -javaagent:/Users/magicLuoMacBook/software/java/apm/apache-skywalking-apm-bin-es7/agent/skywalking-agent.jar -Dskywalking.agent.service_name=c-service -Dskywalking.collector.backend_service=192.168.0.190:11800 -Dskywalking.logging.file_name=a-service-api.log
+ * -javaagent:/Users/magicLuoMacBook/software/java/apm/apache-skywalking-apm-bin-es7/agent/skywalking-agent.jar -Dskywalking.agent.service_name=c-service -Dskywalking.collector.backend_service=192.168.0.190:11800 -Dskywalking.logging.file_name=c-service-api.log
  * @program customer-service
  * @create 2021-04-04
  * @since 1.0
