b3b7953d2f7351c204570068933cf187087ec375
fufeng <15281205719@163.com>
2021-05-03 14:52:03 +0800

    feature: spring aop @After @AfterReturning @AfterThrowing using xml case

--- thinking-in-aop/aop-features/src/main/java/org/lcydream/aop/feature/aspect/xml/AspectXmlConfiguration.java
+++ thinking-in-aop/aop-features/src/main/java/org/lcydream/aop/feature/aspect/xml/AspectXmlConfiguration.java
@@ -1,9 +1,9 @@
 package org.lcydream.aop.feature.aspect.xml;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+
+import java.util.Random;
 
 /**
  * @author luocy
@@ -15,11 +15,36 @@ import org.aspectj.lang.annotation.Pointcut;
 public class AspectXmlConfiguration {
 
     public void beforeAnyPublicMethod() {
+        final Random random = new Random();
+        if (random.nextBoolean()) {
+            throw new RuntimeException("For purpose xml config.");
+        }
         System.out.println("@beforeAnyPublicMethod at any public method.");
     }
 
     public Object aroundAnyPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("@aroundAnyPublicMethod at any public method." + pjp.getSignature());
         return pjp.proceed();
+    }
+
+    /**
+     * 定义后置通知，利用pointcut执行过滤方法
+     */
+    public void afterAnyPublicMethod() {
+        System.out.println("@afterAnyPublicMethod at any public method.");
+    }
+
+    /**
+     * 定义后置通知，利用pointcut执行过滤方法
+     */
+    public void afterReturningAnyPublicMethod() {
+        System.out.println("@afterReturningAnyPublicMethod at any public method.");
+    }
+
+    /**
+     * 定义后置通知，利用pointcut执行过滤方法
+     */
+    public void afterThrowingAnyPublicMethod() {
+        System.out.println("@afterThrowingAnyPublicMethod at any public method.");
     }
 }
