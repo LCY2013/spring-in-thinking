697d265073f049bc8b9db965c2d4a544f6d74d04
fufeng <15281205719@163.com>
2021-05-03 18:31:39 +0800

    feature: spring aop JDK正则 pointcut - JdkRegexpMethodPointcut

---
+++ thinking-in-aop/aop-features/src/main/java/org/lcydream/aop/feature/pointcut/JdkRegexpMethodPointcutInfo.java
@@ -0,0 +1,18 @@
+package org.lcydream.aop.feature.pointcut;
+
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
+
+/**
+ * @author luocy
+ * @description JDK 正则 Pointcut
+ * @program customer-service
+ * @create 2021-05-03
+ * @since 1.0
+ */
+public class JdkRegexpMethodPointcutInfo {
+
+    public static void main(String[] args) {
+        final JdkRegexpMethodPointcut pointcut = new JdkRegexpMethodPointcut();
+    }
+
+}
