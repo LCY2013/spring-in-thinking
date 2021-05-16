88abcaf1d1b36d42bdd17a0d857e5c35c9406366
fufeng <15281205719@163.com>
2021-05-16 22:28:21 +0800

    feature: orm 复杂使用

--- spring-in-action/spring-in-orm/spring-orm-jpa-complex/src/main/java/org/fufeng/project/orm/jpa/complex/OrmComplexJpaApplication.java
+++ spring-in-action/spring-in-orm/spring-orm-jpa-complex/src/main/java/org/fufeng/project/orm/jpa/complex/OrmComplexJpaApplication.java
@@ -8,7 +8,6 @@ import org.fufeng.project.orm.jpa.complex.repository.CoffeeOrderRepository;
 import org.fufeng.project.orm.jpa.complex.repository.CoffeeRepository;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,81 +23,90 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * jpa repository 创建相关
+ *
+ * @see org.springframework.data.jpa.repository.config.JpaRepositoriesRegistrar
+ * @see org.springframework.data.repository.config.RepositoryBeanDefinitionRegistrarSupport
+ * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport
+ * @see org.springframework.data.jpa.repository.support.JpaRepositoryFactory
+ * @see org.springframework.data.repository.core.support.RepositoryFactorySupport
+ */
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
 @Slf4j
 public class OrmComplexJpaApplication implements ApplicationRunner {
-	@Resource
-	private CoffeeRepository coffeeRepository;
-	@Resource
-	private CoffeeOrderRepository orderRepository;
+    @Resource
+    private CoffeeRepository coffeeRepository;
+    @Resource
+    private CoffeeOrderRepository orderRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrmComplexJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrmComplexJpaApplication.class, args);
+    }
 
-	@Override
-	@Transactional
-	public void run(ApplicationArguments args) throws Exception {
-		initOrders();
-		findOrders();
-	}
+    @Override
+    @Transactional
+    public void run(ApplicationArguments args) throws Exception {
+        initOrders();
+        findOrders();
+    }
 
-	private void initOrders() {
-		Coffee latte = Coffee.builder().name("latte")
-				.price(Money.of(CurrencyUnit.of("CNY"), 30.0))
-				.build();
-		coffeeRepository.save(latte);
-		log.info("Coffee: {}", latte);
+    private void initOrders() {
+        Coffee latte = Coffee.builder().name("latte")
+                .price(Money.of(CurrencyUnit.of("CNY"), 30.0))
+                .build();
+        coffeeRepository.save(latte);
+        log.info("Coffee: {}", latte);
 
-		Coffee espresso = Coffee.builder().name("espresso")
-				.price(Money.of(CurrencyUnit.of("CNY"), 20.0))
-				.build();
-		coffeeRepository.save(espresso);
-		log.info("Coffee: {}", espresso);
+        Coffee espresso = Coffee.builder().name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .build();
+        coffeeRepository.save(espresso);
+        log.info("Coffee: {}", espresso);
 
-		CoffeeOrder order = CoffeeOrder.builder()
-				.customer("Li Lei")
-				.items(Collections.singletonList(espresso))
-				.state(OrderState.INIT)
-				.build();
-		orderRepository.save(order);
-		log.info("Order: {}", order);
+        CoffeeOrder order = CoffeeOrder.builder()
+                .customer("Li Lei")
+                .items(Collections.singletonList(espresso))
+                .state(OrderState.INIT)
+                .build();
+        orderRepository.save(order);
+        log.info("Order: {}", order);
 
-		order = CoffeeOrder.builder()
-				.customer("Li Lei")
-				.items(Arrays.asList(espresso, latte))
-				.state(OrderState.INIT)
-				.build();
-		orderRepository.save(order);
-		log.info("Order: {}", order);
-	}
+        order = CoffeeOrder.builder()
+                .customer("Li Lei")
+                .items(Arrays.asList(espresso, latte))
+                .state(OrderState.INIT)
+                .build();
+        orderRepository.save(order);
+        log.info("Order: {}", order);
+    }
 
-	private void findOrders() {
-		coffeeRepository
-				.findAll(Sort.by(Sort.Direction.DESC, "id"))
-				.forEach(c -> log.info("Loading {}", c));
+    private void findOrders() {
+        coffeeRepository
+                .findAll(Sort.by(Sort.Direction.DESC, "id"))
+                .forEach(c -> log.info("Loading {}", c));
 
-		List<CoffeeOrder> list = orderRepository.findTop3ByOrderByUpdateTimeDescIdAsc();
-		log.info("findTop3ByOrderByUpdateTimeDescIdAsc: {}", getJoinedOrderId(list));
+        List<CoffeeOrder> list = orderRepository.findTop3ByOrderByUpdateTimeDescIdAsc();
+        log.info("findTop3ByOrderByUpdateTimeDescIdAsc: {}", getJoinedOrderId(list));
 
-		list = orderRepository.findByCustomerOrderById("Li Lei");
-		log.info("findByCustomerOrderById: {}", getJoinedOrderId(list));
+        list = orderRepository.findByCustomerOrderById("Li Lei");
+        log.info("findByCustomerOrderById: {}", getJoinedOrderId(list));
 
-		// 不开启事务会因为没Session而报LazyInitializationException
-		list.forEach(o -> {
-			log.info("Order {}", o.getId());
-			o.getItems().forEach(i -> log.info("  Item {}", i));
-		});
+        // 不开启事务会因为没Session而报LazyInitializationException
+        list.forEach(o -> {
+            log.info("Order {}", o.getId());
+            o.getItems().forEach(i -> log.info("  Item {}", i));
+        });
 
-		list = orderRepository.findByItems_Name("latte");
-		log.info("findByItems_Name: {}", getJoinedOrderId(list));
-	}
+        list = orderRepository.findByItems_Name("latte");
+        log.info("findByItems_Name: {}", getJoinedOrderId(list));
+    }
 
-	private String getJoinedOrderId(List<CoffeeOrder> list) {
-		return list.stream().map(o -> o.getId().toString())
-				.collect(Collectors.joining(","));
-	}
+    private String getJoinedOrderId(List<CoffeeOrder> list) {
+        return list.stream().map(o -> o.getId().toString())
+                .collect(Collectors.joining(","));
+    }
 }
 
