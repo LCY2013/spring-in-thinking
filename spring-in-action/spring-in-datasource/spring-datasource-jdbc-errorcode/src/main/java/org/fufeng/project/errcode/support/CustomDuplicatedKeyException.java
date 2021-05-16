289fe0c4d9f5ff2018b6ae87ff06bff421c3eaa2
fufeng <15281205719@163.com>
2021-05-16 18:05:32 +0800

    feature: spring jdbc 自定义错误码处理异常

---
+++ spring-in-action/spring-in-datasource/spring-datasource-jdbc-errorcode/src/main/java/org/fufeng/project/errcode/support/CustomDuplicatedKeyException.java
@@ -0,0 +1,13 @@
+package org.fufeng.project.errcode.support;
+
+import org.springframework.dao.DuplicateKeyException;
+
+public class CustomDuplicatedKeyException extends DuplicateKeyException {
+    public CustomDuplicatedKeyException(String msg) {
+        super(msg);
+    }
+
+    public CustomDuplicatedKeyException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
---
+++ spring-in-action/spring-in-datasource/spring-datasource-jdbc-errorcode/src/main/java/org/fufeng/project/errcode/support/ErrorCodeDemoApplication.java
@@ -0,0 +1,12 @@
+package org.fufeng.project.errcode.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorCodeDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorCodeDemoApplication.class, args);
+	}
+}
+
