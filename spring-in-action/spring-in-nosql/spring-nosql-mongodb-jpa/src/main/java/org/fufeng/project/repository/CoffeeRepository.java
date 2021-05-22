78988b9a8d5e7049c25edac067e01f3f090cd222
fufeng <15281205719@163.com>
2021-05-22 15:32:43 +0800

    feature: mongo jpa

---
+++ spring-in-action/spring-in-nosql/spring-nosql-mongodb-jpa/src/main/java/org/fufeng/project/repository/CoffeeRepository.java
@@ -0,0 +1,10 @@
+package org.fufeng.project.repository;
+
+import org.fufeng.project.model.Coffee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends MongoRepository<Coffee, String> {
+    List<Coffee> findByName(String name);
+}
