6daa7503a80a198a6179e922a0ddd6c644305137
fufeng <15281205719@163.com>
2021-05-22 18:02:00 +0800

    feature: redis cache repository

---
+++ spring-in-action/spring-in-cache/spring-cache-redis-repository/src/main/java/org/fufeng/project/repository/CoffeeCacheRepository.java
@@ -0,0 +1,10 @@
+package org.fufeng.project.repository;
+
+import org.fufeng.project.model.CoffeeCache;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CoffeeCacheRepository extends CrudRepository<CoffeeCache, Long> {
+    Optional<CoffeeCache> findOneByName(String name);
+}
---
+++ spring-in-action/spring-in-cache/spring-cache-redis-repository/src/main/java/org/fufeng/project/service/CoffeeOrderService.java
@@ -0,0 +1,44 @@
+package org.fufeng.project.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fufeng.project.model.Coffee;
+import org.fufeng.project.model.CoffeeOrder;
+import org.fufeng.project.model.OrderState;
+import org.fufeng.project.repository.CoffeeOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Slf4j
+@Service
+@Transactional
+public class CoffeeOrderService {
+    @Resource
+    private CoffeeOrderRepository orderRepository;
+
+    public CoffeeOrder createOrder(String customer, Coffee... coffee) {
+        CoffeeOrder order = CoffeeOrder.builder()
+                .customer(customer)
+                .items(new ArrayList<>(Arrays.asList(coffee)))
+                .state(OrderState.INIT)
+                .build();
+        CoffeeOrder saved = orderRepository.save(order);
+        log.info("New Order: {}", saved);
+        return saved;
+    }
+
+    public boolean updateState(CoffeeOrder order, OrderState state) {
+        if (state.compareTo(order.getState()) <= 0) {
+            log.warn("Wrong State order: {}, {}", state, order.getState());
+            return false;
+        }
+        order.setState(state);
+        orderRepository.save(order);
+        log.info("Updated Order: {}", order);
+        return true;
+    }
+}
