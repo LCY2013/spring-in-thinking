e05474e2c6f2c1e8d379be0563b0acf339a51f0b
fufeng <15281205719@163.com>
2021-05-22 21:52:34 +0800

    feature: reactor mongo

---
+++ spring-in-action/spring-in-reactor/spring-reactor-mongo/src/main/java/org/fufeng/project/model/Coffee.java
@@ -0,0 +1,21 @@
+package org.fufeng.project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.money.Money;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Coffee {
+    private String id;
+    private String name;
+    private Money price;
+    private Date createTime;
+    private Date updateTime;
+}
