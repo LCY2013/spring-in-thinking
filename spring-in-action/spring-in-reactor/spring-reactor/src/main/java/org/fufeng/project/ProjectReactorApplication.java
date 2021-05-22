e79a1ddbf36d88f61a97f18d5df6a44472deb2ee
fufeng <15281205719@163.com>
2021-05-22 21:06:29 +0800

    feature: project reactor

--- spring-in-action/spring-in-reactor/spring-reactor/src/main/java/org/fufeng/project/ProjectReactorApplication.java
+++ spring-in-action/spring-in-reactor/spring-reactor/src/main/java/org/fufeng/project/ProjectReactorApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
