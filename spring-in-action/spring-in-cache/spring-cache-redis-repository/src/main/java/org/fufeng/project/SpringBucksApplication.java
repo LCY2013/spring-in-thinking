a063395415e283b06e155cf92acbd65e72090adf
fufeng <15281205719@163.com>
2021-05-22 20:10:24 +0800

    feature: redis cache repository

--- spring-in-action/spring-in-cache/spring-cache-redis-repository/src/main/java/org/fufeng/project/SpringBucksApplication.java
+++ spring-in-action/spring-in-cache/spring-cache-redis-repository/src/main/java/org/fufeng/project/SpringBucksApplication.java
@@ -6,7 +6,6 @@ import org.fufeng.project.converter.BytesToMoneyConverter;
 import org.fufeng.project.converter.MoneyToBytesConverter;
 import org.fufeng.project.model.Coffee;
 import org.fufeng.project.service.CoffeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,12 +15,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.core.convert.RedisCustomConversions;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.repository.configuration.RedisRepositoryConfigurationExtension;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.Optional;
 
+/**
+ * @see RedisRepositoryConfigurationExtension
+ */
 @Slf4j
 @EnableTransactionManagement
 @SpringBootApplication
