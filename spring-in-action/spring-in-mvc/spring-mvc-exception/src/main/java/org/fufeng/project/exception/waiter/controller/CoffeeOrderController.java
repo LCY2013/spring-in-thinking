c40e626ba50eb337f7146c7e01bc8748e2b310a1
fufeng <15281205719@163.com>
2021-05-23 22:32:22 +0800

    feature: spring mvc exception handle

---
+++ spring-in-action/spring-in-mvc/spring-mvc-exception/src/main/java/org/fufeng/project/exception/waiter/controller/CoffeeOrderController.java
@@ -0,0 +1,43 @@
+package org.fufeng.project.exception.waiter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fufeng.project.exception.waiter.controller.request.NewOrderRequest;
+import org.fufeng.project.exception.waiter.model.Coffee;
+import org.fufeng.project.exception.waiter.model.CoffeeOrder;
+import org.fufeng.project.exception.waiter.service.CoffeeOrderService;
+import org.fufeng.project.exception.waiter.service.CoffeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class CoffeeOrderController {
+    @Autowired
+    private CoffeeOrderService orderService;
+    @Autowired
+    private CoffeeService coffeeService;
+
+    @GetMapping("/{id}")
+    public CoffeeOrder getOrder(@PathVariable("id") Long id) {
+        return orderService.get(id);
+    }
+
+    @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public CoffeeOrder create(@RequestBody NewOrderRequest newOrder) {
+        log.info("Receive new Order {}", newOrder);
+        Coffee[] coffeeList = coffeeService.getCoffeeByName(newOrder.getItems())
+                .toArray(new Coffee[] {});
+        return orderService.createOrder(newOrder.getCustomer(), coffeeList);
+    }
+}
---
+++ spring-in-action/spring-in-mvc/spring-mvc-exception/src/main/java/org/fufeng/project/exception/waiter/model/OrderState.java
@@ -0,0 +1,5 @@
+package org.fufeng.project.exception.waiter.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
