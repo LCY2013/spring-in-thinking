3ae7eb482ab86a626ac5d53996d75c39452ae674
fufeng <15281205719@163.com>
2021-05-30 15:54:47 +0800

    feature: spring hateoas client

---
+++ spring-in-action/spring-in-hateoas/spring-hateoas-customer-service/src/main/java/org/fufeng/project/customer/model/CoffeeOrder.java
@@ -0,0 +1,20 @@
+package org.fufeng.project.customer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoffeeOrder {
+    private Long id;
+    private String customer;
+    private OrderState state;
+    private Date createTime;
+    private Date updateTime;
+}
