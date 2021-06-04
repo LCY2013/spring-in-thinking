0fd523da3d59de9368727b7408b3d2e1926c0951
fufeng <15281205719@163.com>
2021-06-04 15:06:48 +0800

    feature: SpringCloud call for feign

---
+++ spring-in-action/spring-in-cloud/spring-cloud-feign-customer-service/src/main/java/org/fufeng/project/feign/customer/CustomerRunner.java
@@ -0,0 +1,51 @@
+package org.fufeng.project.feign.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fufeng.project.feign.customer.integration.CoffeeOrderService;
+import org.fufeng.project.feign.customer.integration.CoffeeService;
+import org.fufeng.project.feign.customer.model.Coffee;
+import org.fufeng.project.feign.customer.model.CoffeeOrder;
+import org.fufeng.project.feign.customer.model.NewOrderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Slf4j
+public class CustomerRunner implements ApplicationRunner {
+    @Autowired
+    private CoffeeService coffeeService;
+    @Autowired
+    private CoffeeOrderService coffeeOrderService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        readMenu();
+        Long id = orderCoffee();
+        queryOrder(id);
+    }
+
+    private void readMenu() {
+        List<Coffee> coffees = coffeeService.getAll();
+        coffees.forEach(c -> log.info("Coffee: {}", c));
+    }
+
+    private Long orderCoffee() {
+        NewOrderRequest orderRequest = NewOrderRequest.builder()
+                .customer("Li Lei")
+                .items(Arrays.asList("capuccino"))
+                .build();
+        CoffeeOrder order = coffeeOrderService.create(orderRequest);
+        log.info("Order ID: {}", order.getId());
+        return order.getId();
+    }
+
+    private void queryOrder(Long id) {
+        CoffeeOrder order = coffeeOrderService.getOrder(id);
+        log.info("Order: {}", order);
+    }
+}
---
+++ spring-in-action/spring-in-cloud/spring-cloud-feign-customer-service/src/main/java/org/fufeng/project/feign/customer/model/OrderState.java
@@ -0,0 +1,5 @@
+package org.fufeng.project.feign.customer.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
