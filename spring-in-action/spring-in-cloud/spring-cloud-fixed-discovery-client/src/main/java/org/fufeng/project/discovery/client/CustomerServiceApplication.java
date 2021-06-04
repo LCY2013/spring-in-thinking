05c1fa034a87dbe6ef5f140687838860375c0c5e
fufeng <15281205719@163.com>
2021-06-04 18:06:56 +0800

    feature: SpringCloud DiscoveryClient

---
+++ spring-in-action/spring-in-cloud/spring-cloud-fixed-discovery-client/src/main/java/org/fufeng/project/discovery/client/CustomerServiceApplication.java
@@ -0,0 +1,74 @@
+package org.fufeng.project.discovery.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.fufeng.project.discovery.client.support.CustomConnectionKeepAliveStrategy;
+import org.fufeng.project.discovery.client.support.FixedDiscoveryClient;
+import org.fufeng.project.discovery.client.support.FixedServerList;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+@Slf4j
+@EnableDiscoveryClient
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    public DiscoveryClient fixedDiscoveryClient() {
+        return new FixedDiscoveryClient();
+    }
+
+    @Bean
+    public FixedServerList fixedServerList() {
+        return new FixedServerList();
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory requestFactory() {
+        PoolingHttpClientConnectionManager connectionManager =
+                new PoolingHttpClientConnectionManager(30, TimeUnit.SECONDS);
+        connectionManager.setMaxTotal(200);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .evictIdleConnections(30, TimeUnit.SECONDS)
+                .disableAutomaticRetries()
+                // 有 Keep-Alive 认里面的值，没有的话永久有效
+                //.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
+                // 换成自定义的
+                .setKeepAliveStrategy(new CustomConnectionKeepAliveStrategy())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return requestFactory;
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMillis(100))
+                .setReadTimeout(Duration.ofMillis(500))
+                .requestFactory(this::requestFactory)
+                .build();
+    }
+}
---
+++ spring-in-action/spring-in-cloud/spring-cloud-fixed-discovery-client/src/main/java/org/fufeng/project/discovery/client/model/OrderState.java
@@ -0,0 +1,5 @@
+package org.fufeng.project.discovery.client.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
