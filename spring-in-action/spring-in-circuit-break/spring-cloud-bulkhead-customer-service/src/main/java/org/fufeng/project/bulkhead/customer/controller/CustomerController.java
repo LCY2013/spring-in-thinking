d86cb2912ff5e04b64667d0d395ca726470fab07
fufeng <15281205719@163.com>
2021-06-07 11:11:37 +0800

    feature: SpringCloud CircuitBreak resilience4j Bulkhead

---
+++ spring-in-action/spring-in-circuit-break/spring-cloud-bulkhead-customer-service/src/main/java/org/fufeng/project/bulkhead/customer/controller/CustomerController.java
@@ -0,0 +1,65 @@
+package org.fufeng.project.bulkhead.customer.controller;
+
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadFullException;
+import io.github.resilience4j.bulkhead.BulkheadRegistry;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.vavr.control.Try;
+import lombok.extern.slf4j.Slf4j;
+import org.fufeng.project.bulkhead.customer.integration.CoffeeOrderService;
+import org.fufeng.project.bulkhead.customer.integration.CoffeeService;
+import org.fufeng.project.bulkhead.customer.model.Coffee;
+import org.fufeng.project.bulkhead.customer.model.CoffeeOrder;
+import org.fufeng.project.bulkhead.customer.model.NewOrderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+@Slf4j
+public class CustomerController {
+    @Autowired
+    private CoffeeService coffeeService;
+    @Autowired
+    private CoffeeOrderService coffeeOrderService;
+    private CircuitBreaker circuitBreaker;
+    private Bulkhead bulkhead;
+
+    public CustomerController(CircuitBreakerRegistry circuitBreakerRegistry,
+                              BulkheadRegistry bulkheadRegistry) {
+        circuitBreaker = circuitBreakerRegistry.circuitBreaker("menu");
+        bulkhead = bulkheadRegistry.bulkhead("menu");
+    }
+
+    @GetMapping("/menu")
+    public List<Coffee> readMenu() {
+        return Try.ofSupplier(
+                Bulkhead.decorateSupplier(bulkhead,
+                        CircuitBreaker.decorateSupplier(circuitBreaker,
+                                () -> coffeeService.getAll())))
+                .recover(BulkheadFullException.class, Collections.emptyList())
+                .recover(Exception.class, Collections.emptyList())
+                .get();
+    }
+
+    @PostMapping("/order")
+    @io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker(name = "order")
+    @io.github.resilience4j.bulkhead.annotation.Bulkhead(name = "order")
+    public CoffeeOrder createOrder() {
+        NewOrderRequest orderRequest = NewOrderRequest.builder()
+                .customer("Li Lei")
+                .items(Collections.singletonList("capuccino"))
+                .build();
+        CoffeeOrder order = coffeeOrderService.create(orderRequest);
+        log.info("Order ID: {}", order != null ? order.getId() : "-");
+        return order;
+    }
+}
---
+++ spring-in-action/spring-in-circuit-break/spring-cloud-bulkhead-customer-service/src/main/java/org/fufeng/project/bulkhead/customer/model/OrderState.java
@@ -0,0 +1,5 @@
+package org.fufeng.project.bulkhead.customer.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
