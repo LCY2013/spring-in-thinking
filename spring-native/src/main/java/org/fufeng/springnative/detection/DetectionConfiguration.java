9831cbdd3c9d61a24b97c7601af4bb7be59f8340
fufeng <15281205719@163.com>
2023-06-10 23:24:05 +0800

    feat: spring native detection native running

---
+++ spring-native/src/main/java/org/fufeng/springnative/detection/DetectionConfiguration.java
@@ -0,0 +1,17 @@
+package org.fufeng.springnative.detection;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.NativeDetector;
+
+@Configuration
+class DetectionConfiguration {
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> detectionListener() {
+        return args -> System.out.println("is native image? " + NativeDetector.inNativeImage());
+    }
+
+}
