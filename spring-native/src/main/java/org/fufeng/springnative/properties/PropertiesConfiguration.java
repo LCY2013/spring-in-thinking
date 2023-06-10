4c240bd1a69dc93d8f97d03b8612c5ee0491492f
fufeng <15281205719@163.com>
2023-06-10 23:21:20 +0800

    feat: spring native property

---
+++ spring-native/src/main/java/org/fufeng/springnative/properties/PropertiesConfiguration.java
@@ -0,0 +1,25 @@
+package org.fufeng.springnative.properties;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(DemoProperties.class)
+class PropertiesConfiguration {
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> propertiesApplicationListener( // <1>
+                                                                              DemoProperties properties) {
+        return args -> System.out.println("the name is " + properties.name());
+    }
+
+}
+
+// <2>
+@ConfigurationProperties(prefix = "bootiful")
+record DemoProperties(String name) {
+}
