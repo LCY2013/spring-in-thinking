ca2817b28693318888c7f92d069ee4d8675d526b
fufeng <15281205719@163.com>
2023-06-10 14:34:33 +0800

    feat: spring native

---
+++ spring-native/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.graalvm.buildtools.native") version "0.9.20"
+}
+
+group = "org.fufeng.native"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    /*developmentOnly("org.springframework.boot:spring-boot-docker-compose")*/
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.graphql:spring-graphql-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ spring-native/src/test/java/org/fufeng/springnative/SpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fufeng.springnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
